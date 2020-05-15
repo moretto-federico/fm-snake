943a640183acdb44d873cc3bd71e5caeb4beddb3
Federico Moretto <federico.moretto@cgn.it>
2020-05-15 09:25:19 +0200

    added increse snake lenght and speed

--- src/components/hooks/useSnakeModel.ts
+++ src/components/hooks/useSnakeModel.ts
@@ -2,9 +2,9 @@ import { useState, useMemo, useEffect, useRef } from "react";
 import { Direction } from "../../share/direction";
 import { Point } from "../../share/point";
 
-function doOnSegment(pointA: Point, pointB: Point, action: (varCoor: 'x' | 'y', fixCoord: 'x' | 'y',dir:Direction) => any) {
-  if(pointA.x === pointB.x) {
-    return action('y', 'x', pointA.y>pointB.y ? Direction.Up:Direction.Down);
+function doOnSegment(pointA: Point, pointB: Point, action: (varCoor: 'x' | 'y', fixCoord: 'x' | 'y', dir: Direction) => any) {
+  if (pointA.x === pointB.x) {
+    return action('y', 'x', pointA.y > pointB.y ? Direction.Up : Direction.Down);
   } else {
     return action('x', 'y', pointA.x > pointB.x ? Direction.Left : Direction.Right);
   }
@@ -71,7 +71,7 @@ class SnakeChain {
     if (Math.max(Math.abs(first.x - second.x), Math.abs(first.y - second.y)) === 1) {
       this.chain.shift();
     } else {
-      doOnSegment(first, second, (vc) => { first[vc] = first[vc] + (first[vc] > second[vc] ? -1 : 1)});
+      doOnSegment(first, second, (vc) => { first[vc] = first[vc] + (first[vc] > second[vc] ? -1 : 1) });
     }
     return this;
   }
@@ -147,12 +147,12 @@ class SnakeCollisionDetector {
       return;
     }
 
-    for (let i = 1; i < chain.length-2; i++) { // it can't be the last two segments!
-      const a = chain[i-1];
+    for (let i = 1; i < chain.length - 2; i++) { // it can't be the last two segments!
+      const a = chain[i - 1];
       const b = chain[i];
-      if(a.x===b.x) {
+      if (a.x === b.x) {
         for (let y = Math.min(a.y, b.y); y < Math.max(a.y, b.y); y++) {
-          if (last.x === a.x && last.y=== y) {
+          if (last.x === a.x && last.y === y) {
             onCollision();
             return;
           }
@@ -164,13 +164,15 @@ class SnakeCollisionDetector {
             return;
           }
         }
-      }     
+      }
     }
   }
 }
 
 export function useSnakeModel(width: number, height: number, onCollision: () => void) {
   const [direction, setDirection] = useState(Direction.Right);
+  const [speed, setSpeed] = useState(500);
+  const tackCounter = useRef(0);
   const [snake, setSnake] = useState({
     chain: [
       { x: Math.ceil(width / 2 - 5), y: Math.ceil(height / 2 - 1) },
@@ -185,30 +187,29 @@ export function useSnakeModel(width: number, height: number, onCollision: () => 
 
   const engine = useMemo(() => new SnakeEngine(snake.chain, setDirection), [snake.chain]);
   useEffect(() => {
-    timer.current.subscribe(() => {
-      setSnake({
-        chain: new SnakeChain(snake.chain)
-          .moveOneFromBegin()
-          .addPointToEnd(direction)
-          .get()
-      })
-    });
-  }, [snake, direction]);
-
-
-  useEffect(() => {
+    let newSnake;
     const interval = setInterval(() => {
-      const newSnake = {
-        chain: new SnakeChain(snake.chain)
-          .moveOneFromBegin()
-          .addPointToEnd(direction)
-          .get()
-      };
+      tackCounter.current = tackCounter.current + 1;
+      if (tackCounter.current % 10 === 0) {
+        setSpeed(s => s * 0.9);
+        newSnake = {
+          chain: new SnakeChain(snake.chain)
+            .addPointToEnd(direction)
+            .get()
+        };
+      } else {
+        newSnake = {
+          chain: new SnakeChain(snake.chain)
+            .moveOneFromBegin()
+            .addPointToEnd(direction)
+            .get()
+        };
+      }
       new SnakeCollisionDetector().detect(snake.chain, width, height, onCollision);
       setSnake(newSnake);
-    }, 500);
+    }, speed);
     return () => clearInterval(interval)
-  }, [snake, direction, width, height, onCollision]);
+  }, [speed, snake, direction, width, height, onCollision]);
 
   return { snake, engine };
 }
